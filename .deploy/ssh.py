990f00a5c48a493caeff582f50d89745609fd94d
Nguyễn Thành Văn <20110224@student.hcmute.edu.vn>
2023-05-11 08:31:37 +0700

    FEAT: Fix ssh

---
+++ .deploy/ssh.py
@@ -0,0 +1,24 @@
+import paramiko
+import os
+import json
+
+SSH_PASSWORD = os.environ['SSH_PASSWORD']
+SSH_IP = os.environ['SSH_IP']
+
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+ssh.connect(SSH_IP, 22, 'root', SSH_PASSWORD)
+
+print(f'Connect to {SSH_IP} OK')
+
+# upload Toeic.war
+ftp = ssh.open_sftp()
+ftp.put('../target/Backend-Toeic-App-0.0.1-SNAPSHOT.war', '/root/BackendToeicAppDeploy/Toeic.war')
+ftp.close()
+
+print(f'Upload Toeic.war ok')
+
+ssh.close()
+
+print(f'Close connection {SSH_IP} OK')
